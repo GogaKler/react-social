a0b5b1e5291d4325b29cba60197eeb1c4529b58f
GogaKler <gogakler@gmail.com>
2022-02-04 23:20:26 +0300

    + components +auto import

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+const Footer = () => {
+	return (
+		<footer>
+			<div className = "footer">
+				<ul>
+					<li>Футер</li>
+				</ul>
+			</div>
+		</footer>
+	)
+}
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+const Header = () => {
+	return (
+		<header>
+			<div className = "header">
+				<ul>
+					<li>Глвная</li>
+					<li>Контакты</li>
+					<li>Услуги</li>
+					<li>О компании</li>
+				</ul>
+			</div>
+		</header>
+	)
+}
+
+export default Header;
