f7265a2766397b160a727b727e894f2ff514ec84
Egor Kolesnikov <gogakler@gmail.com>
2022-07-21 13:40:24 +0300

    refactor_2

--- src/components/AppName/AppName.jsx
+++ src/components/AppName/AppName.jsx
@@ -4,14 +4,26 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { FlexContainer } from "../../components_style/components/Containers/Containers"
 
 const DefaultCustomSize = '22px'
-const customSize = ({font_size}) => font_size || DefaultCustomSize
+const customSize = ({font_size}) => {
+	return font_size || DefaultCustomSize;
+}
 
 const DefaultSizePreset = 'md'
-const xxlSize = ({presets_size}) => presets_size === 'xxl' && css`font-size: 50px;`
-const xlSize = ({presets_size}) => presets_size === 'xl' && css`font-size: 40px;`
-const lgSize = ({presets_size}) => presets_size === 'lg' && css`font-size: 30px;`
-const mdSize = ({presets_size}) => presets_size === 'md' && css`font-size: 22px;`
-const smSize = ({presets_size}) => presets_size === 'sm' && css`font-size: 15px;`
+const xxlSize = ({presets_size}) => {
+	return presets_size === 'xxl' && css`font-size: 50px;`;
+}
+const xlSize = ({presets_size}) => {
+	return presets_size === 'xl' && css`font-size: 40px;`;
+}
+const lgSize = ({presets_size}) => {
+	return presets_size === 'lg' && css`font-size: 30px;`;
+}
+const mdSize = ({presets_size}) => {
+	return presets_size === 'md' && css`font-size: 22px;`;
+}
+const smSize = ({presets_size}) => {
+	return presets_size === 'sm' && css`font-size: 15px;`;
+}
 
 const AppTitle = styled.div`
 	font-weight: 700;
